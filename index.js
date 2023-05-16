29bafc1c344dcdf1fb1459e5cdc9f7cd52d010f2
Mark Anthony <clerigom7@gmail.com>
2023-05-17 01:20:05 +0800

    push

---
+++ index.js
@@ -0,0 +1,155 @@
+(function () {
+  const app = document.querySelector(".app");
+  const socket = io();
+
+  window.onload = function () {
+    window.addEventListener("scroll", function (e) {
+      if (window.pageYOffset > 10) {
+        //100//
+        document.querySelector("header").classList.add("is-scrolling");
+      } else {
+        document.querySelector("header").classList.remove("is-scrolling");
+      }
+    });
+
+    const menu_btn = document.querySelector(".hamburger");
+    const mobile_menu = document.querySelector(".mobile-nav");
+
+    menu_btn.addEventListener("click", function () {
+      menu_btn.classList.toggle("is-active");
+      mobile_menu.classList.toggle("is-active");
+    });
+  };
+
+  app
+    .querySelector(".join-screen #join-user")
+    .addEventListener("click", function () {
+      let username = app.querySelector(".join-screen #username").value;
+      if (username.length == 0) {
+        return;
+      }
+      socket.emit("newuser", username);
+      uname = username;
+      app.querySelector(".join-screen").classList.remove("active");
+      app.querySelector(".chat-screen").classList.add("active");
+    });
+
+  app
+    .querySelector(".chat-screen #send-message")
+    .addEventListener("click", function () {
+      let message = app.querySelector(".chat-screen #message-input").value;
+      if (message.length == 0) {
+        return;
+      }
+      renderMessage("my", {
+        username: uname,
+        text: message,
+      });
+      socket.emit("chat", {
+        username: uname,
+        text: message,
+      });
+      app.querySelector(".chat-screen #message-input").value = "";
+    });
+
+  app
+    .querySelector(".chat-screen #exit-chat")
+    .addEventListener("click", function () {
+      socket.emit("exituser", uname);
+      window.location.href = window.location.href;
+    });
+
+  socket.on("update", function (update) {
+    renderMessage("update", update);
+  });
+  socket.on("chat", function (message) {
+    renderMessage("other", message);
+  });
+
+  // Client-side
+  const messageInput = document.getElementById("message-input");
+  const sendMessageButton = document.getElementById("send-message");
+
+  messageInput.addEventListener("input", function () {
+    if (messageInput.value.trim() === "") {
+      socket.emit("stopTyping");
+    } else {
+      socket.emit("typing");
+    }
+  });
+
+  sendMessageButton.addEventListener("click", function () {
+    const message = messageInput.value;
+    // ... Send the message using your existing code ...
+
+    messageInput.value = ""; // Clear the input field
+    socket.emit("stopTyping"); // Emit "stopTyping" event when the message is sent
+  });
+
+  socket.on("isTyping", function (data) {
+    const typingIndicator = document.getElementById("isTyping");
+    typingIndicator.innerHTML = `<img src="/typing.gif" alt="${data.username} is typing...">`;
+  });
+
+  socket.on("stopTyping", function () {
+    const typingIndicator = document.getElementById("isTyping");
+    typingIndicator.innerHTML = "";
+  });
+
+  function renderMessage(type, message) {
+    let messageContainer = app.querySelector(".chat-screen .messages");
+    let timestamp = Date.now(); // Get the current timestamp in milliseconds
+
+    if (type == "my") {
+      let el = document.createElement("div");
+      el.setAttribute("class", "message my-message");
+      el.innerHTML = `<div><div class ="name">You</div><div class="text">${message.text}</div><div class="timestamp" data-timestamp="${timestamp}"></div></div>`;
+      messageContainer.appendChild(el);
+    } else if (type == "other") {
+      let el = document.createElement("div");
+      el.setAttribute("class", "message other-message");
+      el.innerHTML = `<div><div class ="name">${message.username}</div><div class="text">${message.text}</div><div class="timestamp" data-timestamp="${timestamp}"></div></div>`;
+      messageContainer.appendChild(el);
+    } else if (type == "update") {
+      let el = document.createElement("div");
+      el.setAttribute("class", "update");
+      el.innerText = message;
+      messageContainer.appendChild(el);
+    }
+
+    //scroll to end o the chat
+    messageContainer.scrollTop =
+      messageContainer.scrollHeight - messageContainer.clientHeight;
+  }
+
+  // Update timestamps periodically
+  setInterval(updateTimestamps, 1000);
+
+  function updateTimestamps() {
+    const timestampElements = app.querySelectorAll(".timestamp");
+    timestampElements.forEach((element) => {
+      const timestamp = parseInt(element.getAttribute("data-timestamp"));
+      const elapsed = Math.floor((Date.now() - timestamp) / 1000); // Elapsed time in seconds
+
+      if (elapsed < 5) {
+        // Less than 5 seconds
+        element.innerText = "Just now";
+      } else if (elapsed < 60) {
+        // Less than 1 minute
+        element.innerText = `${elapsed} seconds ago`;
+      } else if (elapsed < 3600) {
+        // Less than 1 hour
+        const minutes = Math.floor(elapsed / 60);
+        element.innerText = `${minutes} minutes ago`;
+      } else {
+        // More than 1 hour
+        const hours = Math.floor(elapsed / 3600);
+        element.innerText = `${hours} hours ago`;
+      }
+    });
+  }
+
+  socket.on("usercnt", function (msg) {
+    document.getElementById("count").innerHTML = msg;
+  });
+})();
---
+++ server.js
@@ -0,0 +1,86 @@
+var express = require("express");
+var app = express();
+const path = require("path");
+
+const http = require("http");
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+var count = 0;
+// const PORT = process.end.PORT || 8000;
+
+const defURL = "http://localhost:8000/";
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("pages/main", {
+    socketURL: defURL,
+  });
+});
+
+app.get("/login", function (req, res) {
+  res.render("pages/login", {
+    socketURL: defURL,
+  });
+});
+
+app.get("/signup", function (req, res) {
+  res.render("pages/signup", {
+    socketURL: defURL,
+  });
+});
+
+app.get("/index", function (req, res) {
+  res.render("pages/index", { socketURL: defURL });
+});
+app.get("/index.js", function (req, res) {
+  res.setHeader("Content-Type", "text/javascript");
+  res.sendFile(path.join(__dirname, "index.js"));
+});
+
+// Server-side
+io.on("connection", function (socket) {
+  console.log("a user connected");
+  count++;
+  io.emit("usercnt", count);
+
+  let username; // Store the username when the user connects
+
+  socket.on("newuser", function (receivedUsername) {
+    username = receivedUsername; // Store the username
+    socket.broadcast.emit("update", username + " joined the conversation");
+  });
+
+  socket.on("disconnect", function () {
+    console.log("a user disconnected");
+    count--;
+    io.emit("usercnt", count);
+
+    if (username) {
+      // Check if the username is available
+      socket.broadcast.emit("update", username + " left the conversation");
+    }
+  });
+
+  socket.on("chat", function (message) {
+    socket.broadcast.emit("chat", message);
+  });
+
+  socket.on("typing", function () {
+    if (username) {
+      socket.broadcast.emit("isTyping", {
+        username,
+        typingIndicatorURL: "/public/typing.gif",
+      });
+    }
+  });
+
+  socket.on("stopTyping", function () {
+    socket.broadcast.emit("stopTyping");
+  });
+});
+
+server.listen(8000);
+console.log("server is listening on port: 8000");
